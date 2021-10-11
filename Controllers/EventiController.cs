8ad4d5918c611e01abdcd1df9419ebe960de2c00
Lambdat <oreficeantonio98@postecert.it>
2021-10-11 12:30:39 +0200

    -Aggiunta Intefaccia ILogger per il Controller

--- Controllers/EventiController.cs
+++ Controllers/EventiController.cs
@@ -10,28 +10,36 @@ namespace CalendarioAPI.Controllers
     [ApiController]
     public class EventiController : ControllerBase
     {
-        //Creaiamo campo di tipo Interfaccia privato a sola lettura
+        //Creaiamo campo di tipo Interfaccia privato a sola lettura del nostro service
         private readonly ICalendarioService<Evento> _iCalendarioService;
 
+        //Creiamo campo di tipo Interfaccia per il Logger che estende il controller
+        private readonly ILogger<EventiController> _iLogger;
 
         //private readonly ILogger<EventiController> _logger;
 
         //Metodo Costruttore per inizializzare il campo di tipo interfaccia
-        public EventiController(ICalendarioService<Evento> _iCalendarioService)
+        public EventiController(ICalendarioService<Evento> _iCalendarioService,
+                                ILogger<EventiController> _iLogger)
         {
             this._iCalendarioService = _iCalendarioService;
+            this._iLogger = _iLogger;
         }
 
         //Metodi GET del protocollo HTTP 
         [HttpGet]
         public List<Evento> Elenco()
         {
+            _iLogger.LogInformation("Elenco attraverso metodo HTTP GET ");
+
             return _iCalendarioService.Elenco();
         }
 
         [HttpGet("{titolo}")] //{titolo} è la wildcard che passerà nel FromRoute
         public Evento Cerca([FromRoute] string titolo)
         {
+            _iLogger.LogInformation("Cerca attraverso metodo HTTP GET ");
+
             return _iCalendarioService.Cerca(titolo);
         }
 
@@ -40,6 +48,8 @@ namespace CalendarioAPI.Controllers
         [HttpPost]
         public IActionResult Aggiungi([FromBody] Evento t)
         {
+            _iLogger.LogInformation("Aggiunto Evento attraverso metodo HTTP POST");
+
             _iCalendarioService.Aggiungi(t);
 
             return Ok();
@@ -50,6 +60,8 @@ namespace CalendarioAPI.Controllers
         [HttpDelete("{titolo}")]  
         public IActionResult Elimina([FromRoute]string titolo) //fromquery => .../eventi?titolo=natale
         {
+            _iLogger.LogInformation("Aggiunto Evento attraverso metodo HTTP DELETE");
+
             _iCalendarioService.Elimina(titolo.ToLower());
 
             return Ok();
@@ -60,6 +72,8 @@ namespace CalendarioAPI.Controllers
         [HttpPut]
         public IActionResult Modifica([FromBody] Evento t)
         {
+            _iLogger.LogInformation("Aggiunto Evento attraverso metodo HTTP PUT");
+
             _iCalendarioService.Modifica(t);
 
             return Ok();
